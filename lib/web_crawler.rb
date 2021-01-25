0d3625455f678b13edcff4c29e3c9f368868d621
George <george.gbenle@outlook.com>
2021-01-25 20:32:25 +0000

    Initialize Web_crawler class with @url and @parsed_page

--- lib/web_crawler.rb
+++ lib/web_crawler.rb
@@ -1,3 +1,13 @@
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
+
+class Web_crawler
+    attr_reader :url, :page_number
+   
+    def initialize
+        @url = 'https://www.airbnb.com/s/Greater-Accra-Region--Ghana/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&date_picker_type=calendar&checkin=2021-02-01&checkout=2021-02-02&source=structured_search_input_header&search_type=pagination&place_id=ChIJc6e3soSQ3w8RYQbYT8_WxlM&federated_search_session_id=21f7da73-547c-4a98-88a0-66c2a6e4cff0&map_toggle=false'
+        unparsed_page = HTTParty.get(url)
+        @parsed_page = Nokogiri::HTML(unparsed_page.body)
+    end
+end
