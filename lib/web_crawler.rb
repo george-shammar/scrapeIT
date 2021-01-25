868f42b741f92d3f232123e7b548acdf86b46b85
George <george.gbenle@outlook.com>
2021-01-25 20:42:57 +0000

    Create crawl method

--- lib/web_crawler.rb
+++ lib/web_crawler.rb
@@ -43,3 +43,22 @@ class Web_crawler
         end
     end
 end
+
+
+
+class Whole_web < Web_crawler
+    attr_reader :ur, :page_number
+
+    def initialize
+        page_number = 20
+        while page_number <= 40 do 
+            @ur_first = 'https://www.airbnb.com/s/Greater-Accra-Region--Ghana/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&date_picker_type=calendar&checkin=2021-02-01&checkout=2021-02-02&source=structured_search_input_header&search_type=pagination&place_id=ChIJc6e3soSQ3w8RYQbYT8_WxlM&federated_search_session_id=e8181d27-6be3-4467-87b5-2d4cb71b65c1&map_toggle=false&items_offset='
+            @ur_last = '&section_offset=3'
+            @ur = "#{@ur_first}#{page_number}#{@ur_last}"
+        unparsed_page = HTTParty.get(@ur)
+        @parsed_page = Nokogiri::HTML(unparsed_page.body)
+        page_number += 20
+        end
+        
+    end
+end
