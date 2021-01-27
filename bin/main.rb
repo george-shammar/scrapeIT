70fa390984da609666e10980101271a43be6a5e4
George <george.gbenle@outlook.com>
2021-01-27 08:46:45 +0000

    Move main.rb into bin directory

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
-require_relative './lib/web_crawler'
-require_relative './lib/whole_web'
+require_relative '../lib/web_crawler'
+require_relative '../lib/whole_web'
 
 scrape = Crawler.new
 scrape.display
